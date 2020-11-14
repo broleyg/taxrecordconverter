12812aef761904491c7332c0248b6818e0d27588
broleyg <broleyg@yahoo.com>
2020-11-14 06:54:06 -0500

    Horrible first stab, need to refactor

--- converter.py
+++ converter.py
@@ -1,28 +1,95 @@
 import re
-
+from taxrecord import TaxRecord
 
 def extract_field(start, length, buffer):
     value = buffer[start:length].strip()
     print('value={}'.format(value))
     return value
 
-def categorize(line):
+def parse_line(line):
+    words = []
+    tokens = re.finditer(r'[\,|\.|\-|\/|\(|\)|\:|\*|\w]+', line)
+    for m in tokens:
+        words.append(m)
+    return words
 
-    if re.match("\ ", line):
-        print('PARSE : {}'.format(line))
-        account = line[0:10].strip()
-        full_name = line[11:25]
-        print('account={}'.format(account))
-        for m in re.finditer(r'\w+', line):
-            print('%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
+def dump_tokens(tokens):
+    for m in tokens:
+        print('%02d-%02d: %s' % (m.start(), m.end(), m.group(0)))
+
+def is_record_line(tokens):
+
+    # All records have a least one field
+    if len(tokens) == 0:
+        return False
+
+    # All records lines start with one or more spaces so the
+    # start position of the first token must be greater than zero
+    first_match = tokens[0]
+    if first_match.start() == 0:
+        return False
+
+    first_string = first_match.group(0)
+    if first_string == "Land":
+        return False
+
+    if first_string == "Paris":
+        return False
+
+    return True
+
+def is_first_record_line(tokens):
+    first_match = tokens[0]
+    if first_match.start() < 8:
+        return True
     else:
-        #print('IGNORE: {}'.format(line))
-        None
+        return False
+
+def is_empty_line(tokens):
+    return len(tokens) == 0
 
 def convert():
+    in_record = False
+    tokens = None
+    tax_record = None
+    records = {}
+    i = 0
+    line_type = '?'
+    num = 0
     with open('tax-records.txt', 'r') as infile:
         for line in infile:
-            categorize(line)
+            i = i + 1
+            line_type = '?'
+            tokens = parse_line(line)
+            if not is_empty_line(tokens):
+                if is_record_line(tokens):
+                    if is_first_record_line(tokens):
+                        line_type = 'F'
+                        in_record = True
+                        #dump_tokens(tokens)
+                        tax_record = TaxRecord(tokens)
+                        records[tax_record.account_num] = tax_record
+                    else:
+                        line_type = '-'
+                        tax_record.update(tokens)
+                else:
+                    line_type = 'x'
+                    if in_record:
+                        in_record = False
+            else:
+                line_type = '.'
+
+            if line_type == 'F':
+                num = num + 1
+                #print('{} {} {} {}'.format(num, i, line_type, line[:-1]))
+
+    print('*'*75)
+    for key, value in records.items():
+        print(value.csv())
+    print('*'*75)
+    print(len(records))
+
+
 
 def main():
     convert()
--- taxrecord.py
+++ taxrecord.py
@@ -1,6 +1,86 @@
+class Position:
+   def __init__(self, start, length, type="String", ignore=None):
+       self.start = start
+       self.end = length
+       self.type = type
+       self.ignore = ignore
+
 class TaxRecord:
 
-    def __init__(self, account_num):
-        self.account_num = account_num
+    def __init__(self, tokens):
+        end = len(tokens) - 1
+        self.account_num = tokens[0].group(0)
+        self.name = tokens[1].group(0)
+        index = 2
+        while index < end - 4:
+            self.name = self.name + ' ' + tokens[index].group(0)
+            index = index + 1
+        self.land_value = float(tokens[end-4].group(0).replace(',',''))
+        self.bulding_value = float(tokens[end-3].group(0).replace(',',''))
+        self.excemption_value = float(tokens[end-2].group(0).replace(',',''))
+        self.assessment_value = float(tokens[end-1].group(0).replace(',',''))
+        self.tax = float(tokens[end].group(0).replace(',',''))
+        self.acres = 0
+        self.map = None
+        self.tree_growth = False
+        self.homestead = False
+        self.tax_rate = 0.00
+        self.land_rate = 0.00
+
+        if self.tax > 0 and self.assessment_value > 0:
+            self.tax_rate = self.tax / self.assessment_value * 100
+
+    def __str__(self):
+        buff = "Account #{}".format(self.account_num)
+        buff = buff + "\n\tName = {}".format(self.name)
+        buff = buff + "\n\tLand = {}".format(self.land_value)
+        buff = buff + "\n\tBuliding = {}".format(self.bulding_value)
+        buff = buff + "\n\tExemption = {}".format(self.excemption_value)
+        buff = buff + "\n\tAssessment = {}".format(self.assessment_value)
+        buff = buff + "\n\tTax = {}".format(self.tax)
+        buff = buff + "\n\tAcres = {}".format(self.acres)
+        buff = buff + "\n\tMap = {}".format(self.map)
+        buff = buff + "\n\tTree Growth = {}".format(self.tree_growth)
+        buff = buff + "\n\tHomestead = {}".format(self.homestead)
+        buff = buff + "\n\tTax Rate = {}".format(self.tax_rate)
+        buff = buff + "\n\tLand Rate = {}".format(self.land_rate)
+
+        return buff
+
+    def csv(self):
+        buff = "{}".format(self.account_num)
+        buff = buff + ", {}".format(self.name)
+        buff = buff + ", {}".format(self.land_value)
+        buff = buff + ", {}".format(self.bulding_value)
+        buff = buff + ", {}".format(self.excemption_value)
+        buff = buff + ", {}".format(self.assessment_value)
+        buff = buff + ", {}".format(self.tax)
+        buff = buff + ", {}".format(self.acres)
+        buff = buff + ", {}".format(self.map)
+        buff = buff + ", {}".format(self.tree_growth)
+        buff = buff + ", {}".format(self.homestead)
+        buff = buff + ", {}".format(self.tax_rate)
+        buff = buff + ", {}".format(self.land_rate)
+
+        return buff
+
+    def update(self, tokens):
+        token_count = len(tokens)
+        if token_count == 1:
+            value = tokens[0].group(0)
+            prefix = value[0:2]
+            if prefix == "R-" or prefix == "U-":
+                self.map = value
+
+        for index in range(token_count):
+            value = tokens[index].group(0)
+            if value == "Acres":
+                self.acres = float(tokens[index+1].group(0))
+                if self.tax > 0 and self.acres > 0:
+                    self.land_rate = self.tax / self.acres
+            if value == "*TREE":
+                self.tree_growth = True
+            if value == "Homestead":
+                self.homestead = True
 
 
